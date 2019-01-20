e98529ce516ce0af4c8370f2f137e0f1c374388b
Michael Uskov <uskovm@gmail.com>
2019-01-21 00:22:25 +0500

    First infra commit

---
+++ index.tsx
@@ -0,0 +1,6 @@
+import {h, render, Component} from "preact";
+require("./index.css");
+
+const App = () => <div>Hello, World!</div>;
+
+render(App, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const isProduction = process.argv.indexOf("production") !== -1;
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const isDevelopment = !isProduction;
+const distPath = path.join(__dirname, '/dist');
+
+
+const config = {
+    entry: {
+        main: './index.tsx'
+    },
+    mode: isDevelopment ? "development" : "production",
+    output: {
+        filename: 'bundle.js',
+        path: distPath
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'ts-loader'
+                }]
+            },
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'babel-loader'
+            }]
+        }, {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [
+                isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                'css-loader'
+            ]
+        }, {
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: 'images/[name][hash].[ext]'
+                }
+            }
+            ],
+        }, {
+            test: /\.(eot|svg|ttf|woff|woff2)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[name][hash].[ext]'
+                }
+            },
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name][hash].css',
+            chunkFilename: '[id][hash].css'
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+    optimization: isProduction ? {
+        minimizer: [
+            new UglifyJsPlugin({
+                sourceMap: false,
+                uglifyOptions: {
+                    compress: {
+                        inline: false,
+                        drop_console: true
+                    },
+                },
+            }),
+        ],
+        splitChunks: {
+            chunks: "all"
+        }
+    } : {},
+    devServer: {
+        contentBase: distPath,
+        port: 9000,
+        compress: true,
+        open: true,
+    }
+};
+
+module.exports = config;
