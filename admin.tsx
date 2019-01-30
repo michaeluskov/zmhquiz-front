f872266717f60e2a89fc06b0c81e8a00d05706fa
Michael Uskov <uskovm@gmail.com>
2019-01-30 22:25:59 +0500

    Admin container

---
+++ admin.tsx
@@ -0,0 +1,4 @@
+import {h, Component, render} from "preact";
+import {AdminContainer} from "./src/admin/adminContainer/AdminContainer";
+
+render(<AdminContainer/>, document.getElementById("root"));
---
+++ src/admin/adminContainer/AdminContainer.tsx
@@ -0,0 +1,7 @@
+import {h, Component} from "preact";
+
+export class AdminContainer extends Component<{}> {
+    render() {
+        return <div></div>;
+    }
+}
