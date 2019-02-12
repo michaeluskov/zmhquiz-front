dbd8887151865dcb996454c93acc6952549afdb0
Michael Uskov <uskovm@gmail.com>
2019-02-12 21:44:29 +0500

    That's all!

--- index.tsx
+++ index.tsx
@@ -1,18 +1,10 @@
 import {h, render, Component} from "preact";
-import {QuestionView} from "./src/views/questionView/QuestionView";
 import {Message} from "./src/views/message/Message";
 import {Container} from "./src/logic/container";
 import 'promise-polyfill/src/polyfill';
 
 require("./index.css");
 
-const quizId = document.location.pathname.slice(1);
-const queryParams: any = document.location.search
-    .slice(1)
-    .split('&')
-    .map(param => param.split('=').map(e => decodeURIComponent(e)))
-    .reduce((prev, param) => ({...prev, [param[0]]: param[1]}), {});
-
-let App = () => <Container login={queryParams.login} hash={queryParams.hash} quizId={quizId} />;
+let App = () => <Message header="Вот и всё, ребята!" />;
 
 render(<App />, document.getElementById("root"));
